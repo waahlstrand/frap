31809034dced55723c96cdfdbe143cf8bf6bdb3a
vws <victor.wahlstrand@gmail.com>
2019-09-11 21:21:59 +0200

    Reformatted code

--- utils.py
+++ utils.py
@@ -1,40 +1,81 @@
 import numpy as np
 import json
 import logging
+from datasets import RecoveryDataset, RecoveryTrainingDataset, RecoveryValidationDataset
+from attrdict import AttrDict
+
+class Configuration(dict):
+    """ Dictionary subclass whose entries can be accessed by attributes
+        (as well as normally).
+    """
+    def __init__(self, *args, **kwargs):
+        super(Configuration, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+    @staticmethod
+    def from_nested_dict(data):
+        """ Construct nested AttrDicts from nested dictionaries. """
+        if isinstance(data, str) and ".json" in data:
+            f = open(data)
+            data = json.load(f)
+            f.close()
+
+            return Configuration({key: Configuration.from_nested_dict(data[key]) for key in data})
+        elif not isinstance(data, dict):
+            return data
+        else:
+            return Configuration({key: Configuration.from_nested_dict(data[key]) for key in data})
 
 
-class Params():
-    """Class that loads hyperparameters from a json file.
+def set_logger(log_path):
+    """Set the logger to log info in terminal and file `log_path`.
+    In general, it is useful to have a logger so that every output to the terminal is saved
+    in a permanent file. Here we save it to `model_dir/train.log`.
     Example:
     ```
-    params = Params(json_path)
-    print(params.learning_rate)
-    params.learning_rate = 0.5  # change the value of learning_rate in params
+    logging.info("Starting training...")
     ```
+    Args:
+        log_path: (string) where to log
     """
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
 
-    def __init__(self, json_path):
-        with open(json_path) as f:
-            params = json.load(f)
-            self.__dict__.update(params)
+    if not logger.handlers:
+        # Logging to a file
+        file_handler = logging.FileHandler(log_path, mode='w')
+        file_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s: %(message)s'))
+        logger.addHandler(file_handler)
 
-    def save(self, json_path):
-        with open(json_path, 'w') as f:
-            json.dump(self.__dict__, f, indent=4)
-            
-    def update(self, json_path):
-        """Loads parameters from json file"""
-        with open(json_path) as f:
-            params = json.load(f)
-            self.__dict__.update(params)
-
-    @property
-    def dict(self):
-        """Gives dict-like access to Params instance by `params.dict['learning_rate']"""
-        return self.__dict__
+        # Logging to console
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(logging.Formatter('%(message)s'))
+        logger.addHandler(stream_handler)
 
 def output_size_from_conv(in_size, kernel_size, stride = 1, padding = 0, dilation = 1):
     
     size = np.floor( (in_size+ 2*padding-dilation*(kernel_size-1)-1)/stride +1 )
     
     return int(size)
+
+def str_to_bool(s):
+    if s == 'True':
+         return True
+    elif s == 'False':
+         return False
+    else:
+         raise ValueError 
+
+def get_dataloaders(mode, data_path, validation):
+
+    if mode == "rcs":
+        if validation:
+            train = RecoveryTrainingDataset(data_path)
+            val = RecoveryValidationDataset(data_path)
+
+            return train, val
+
+        else:
+            train = RecoveryTrainingDataset(data_path)
+
+            return train, None
